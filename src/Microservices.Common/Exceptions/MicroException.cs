08a2557b80a1ad305d98f3c8c0d316682ca4ad22
Audry Kibonge <audrykibs@live.fr>
2024-11-08 11:15:51 -0600

    custom exception handler

---
+++ src/Microservices.Common/Exceptions/MicroException.cs
@@ -0,0 +1,31 @@
+﻿namespace Microservices.Common.Exceptions
+{
+    public class MicroException : Exception
+    {
+        public string Code { get; }
+
+        public MicroException()
+        {
+        }
+
+        public MicroException(string code)
+        {
+            Code = code;
+        }
+
+        public MicroException(string message, params object[] args) : this(string.Empty, message, args)
+        {
+        }
+
+        public MicroException(string code, string message, params object[] args) : this(null, code, message, args)
+        {
+        }
+        public MicroException(Exception innerException, string message, params object[] args) : this(innerException, string.Empty, message, args)
+        {
+        }
+        public MicroException(Exception innerException, string code, string message, params object[] args) : base(string.Format(message, args), innerException)
+        {
+            Code = code;
+        }
+    }
+}
